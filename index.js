d7d941e37fc6a99c1f4bea4c1323cd44e98f4af8
ownyanteh <ownyanteh@gmail.com>
2024-10-30 11:17:44 +0000

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-import express, { text } from "express";
+import express from "express";
+import mongoose from "mongoose";
+
+// Initialize Express app
+const app = express();
+
+// Connect to MongoDB
+await mongoose.connect(process.env.MONGO_URI);
+
+// Start server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
